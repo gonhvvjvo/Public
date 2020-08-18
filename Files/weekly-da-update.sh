1fa64b0bceecb0d52ae37792ce4e56fb9baa741d
gonhvvjvo <gonhvvjvo@gmail.com>
2020-08-18 12:19:37 +0700

    Update

---
+++ Files/weekly-da-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+########## Update all ##########
+#cd /usr/local/directadmin/custombuild && \
+#./build update && \
+#./build all d
+
+
+########## Update version ##########
+cd /usr/local/directadmin/custombuild && \
+./build update_versions
---
+++ Files/weekly-yum-update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+########## Update all ##########
+yum update -y && yum upgrade -y && reboot
