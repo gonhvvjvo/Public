67f39192fa891015e7b04de7e82315a622beb0cc
gonhvvjvo <gonhvvjvo@gmail.com>
2020-08-18 14:45:09 +0700

    Update

--- Files/weekly-da-update.sh
+++ Files/weekly-da-update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ########## Update all ##########
-#cd /usr/local/directadmin/custombuild && \
-#./build update && \
+#cd /usr/local/directadmin/custombuild
+#./build update
 #./build all d
 
 
 ########## Update only new version ##########
-cd /usr/local/directadmin/custombuild && \
+cd /usr/local/directadmin/custombuild
 ./build update_versions
